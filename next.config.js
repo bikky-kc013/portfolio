d18e126b88dceb2c6bb6a4e959156b8c710a5ea9
bikky-kc013 <bharathkc013@gmail.com>
2025-02-13 21:06:12 +0545

    feat:build fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  images: { unoptimized: true },
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return 'my-build-id'
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
